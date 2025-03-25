d04abb987b0e3c7a257415b371ca61292863e669
Khắc Bảo <caokhacbao0712@gmail.com>
2025-03-25 16:26:17 +0700

    fix image

--- models/blogModel.js
+++ models/blogModel.js
@@ -12,13 +12,12 @@ let blogSchema = new mongoose.Schema(
       required: true,
     },
     // Cập nhật hình ảnh giống Product schema
-    image: [
-      {
-        public_id: { type: String, required: true },
-        asset_id: { type: String, required: true },
-        url: { type: String, required: true },
-      },
-    ],
+    images: [
+			{
+				public_id: String,
+				url: String,
+			},
+		],
   },
   { timestamps: true } // Tự động thêm các trường createdAt và updatedAt
 );
